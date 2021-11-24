b74e88fb3ba7eeb7f8b9188b1f7c337561980638
Jordan Miles <jordanamiles@hotmail.com>
2021-11-24 03:54:52 -0600

    Initialized repo and started basic UI layout

---
+++ src/app/todo-item/todo-item.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Todo } from '../models/todo.model';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+
+  @Input()
+  todo!: Todo;
+
+  @Output() todoClicked: EventEmitter<void> = new EventEmitter()
+  @Output() editClicked: EventEmitter<void> = new EventEmitter()
+  @Output() deleteClicked: EventEmitter<void> = new EventEmitter()
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  prefix: '',
+  purge: {
+    enabled: process.env.WEBPACK_DEV_SERVER === 'true' && process.argv.join(' ').indexOf('build') !== -1,
+    content: [
+      "./src/**/*.{html,ts}",
+      "./projects/**/*.{html,ts}"
+    ]
+  },
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+};
